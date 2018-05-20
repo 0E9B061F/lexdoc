6a1d96e6f8568a2c8a7db55fe28a478934bab01b
Macquarie Sharpless <macquarie.sharpless@gmail.com>
2018-05-20 00:00:48 -0400

    Test build and dependency errors

--- spec/lexdoc-spec.js
+++ spec/lexdoc-spec.js
@@ -4,16 +4,62 @@ const Errors = require('../lib/errors.js')
 
 
 describe('lexdoc', function() {
+  let LD
+
+  beforeEach(function() {
+    LD = require('../index.js')
+  })
 
   it('should not build without a token definition', function() {
-    const LD = require('../index.js')
     expect(LD.build).toThrowError(Errors.BuildError)
   })
 
   it('should not accept an empty token definition', function() {
-    const LD = require('../index.js')
     function emptydef() { LD.build({}) }
     expect(emptydef).toThrowError(Errors.BuildError)
+  })
+
+  it('should not accept a single mode definition after modes have been defined', function() {
+    function mixedbuild() {
+      LD.mode('A', { TokenA: 'A' })
+      LD.mode('B', { TokenA: 'B' })
+      LD.build({ TokenC: 'C' })
+    }
+    expect(mixedbuild).toThrowError(Errors.BuildError)
+  })
+
+  it('should detect dependency loops', function() {
+    function catloop() {
+      LD.build({
+        Foo: {pattern: LD.CATEGORY, categories: 'Bar'},
+        Bar: {pattern: LD.CATEGORY, categories: 'Baz'},
+        Baz: {pattern: LD.CATEGORY, categories: 'Foo'}
+      })
+    }
+    function altloop() {
+      LD.build({
+        Foo: {pattern: 'foo', longer_alt: 'Bar'},
+        Bar: {pattern: 'bar', longer_alt: 'Baz'},
+        Baz: {pattern: 'baz', longer_alt: 'Foo'}
+      })
+    }
+    expect(catloop).toThrowError(Errors.DependencyError)
+    expect(altloop).toThrowError(Errors.DependencyError)
+  })
+
+  it('should detect missing dependencies', function() {
+    function missingcat() {
+      LD.build({
+        Foo: LD.CATEGORY,
+        Bar: LD.CATEGORY,
+        Bat: {pattern: 'bat', categories: 'Foo Bar Baz'}
+      })
+    }
+    function missingalt() {
+      LD.build({ Foo: {pattern: 'foo', longer_alt: 'Bar'} })
+    }
+    expect(missingcat).toThrowError(Errors.DependencyError)
+    expect(missingalt).toThrowError(Errors.DependencyError)
   })
 
 })
