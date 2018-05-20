f40e5d0f400c46691c6a22b7418558e40ed311ce
Macquarie Sharpless <macquarie.sharpless@gmail.com>
2018-05-20 03:16:39 -0400

    Organized tests

--- spec/lexdoc-spec.js
+++ spec/lexdoc-spec.js
@@ -11,93 +11,99 @@ describe('lexdoc', function() {
     LD = new Lexdoc()
   })
 
-  it('should not build without a token definition', function() {
-    function noarg() { LD.build() }
-    expect(noarg).toThrowError(Errors.BuildError)
-  })
+  describe('single mode', function() {
+    it('should not build without a token definition', function() {
+      function noarg() { LD.build() }
+      expect(noarg).toThrowError(Errors.BuildError)
+    })
 
-  it('should not accept an empty token definition', function() {
-    function emptydef() { LD.build({}) }
-    expect(emptydef).toThrowError(Errors.BuildError)
-  })
+    it('should not accept an empty token definition', function() {
+      function emptydef() { LD.build({}) }
+      expect(emptydef).toThrowError(Errors.BuildError)
+    })
 
-  it('should not accept a single mode definition after modes have been defined', function() {
-    function mixedbuild() {
-      LD.mode('A', { TokenA: 'A' })
-      LD.mode('B', { TokenA: 'B' })
-      LD.build({ TokenC: 'C' })
-    }
-    expect(mixedbuild).toThrowError(Errors.BuildError)
-  })
-
-  it('should require at least two modes', function() {
-    function onemode() {
-      LD.mode('A', { TokenA: 'A' })
-      LD.setDefault('A')
-      LD.build()
-    }
-    expect(onemode).toThrowError(Errors.BuildError)
-  })
-
-  it('should ensure that the default mode is valid', function() {
-    function invalidDefault() {
-      LD.mode('A', { TokenA: {pattern: 'A', line_breaks: true} })
-      LD.mode('B', { TokenB: 'B' })
-      LD.setDefault('C')
-      LD.build()
-    }
-    expect(invalidDefault).toThrowError(Errors.BuildError)
-  })
-
-  it('should support multiple modes', function() {
-    const numbers = [2,3,4,10,20]
-    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    numbers.forEach((number)=> {
-      LD = new Lexdoc()
-      LD.setDefault('A')
-      for (let count = 0; count < number; count++) {
-        const letter = letters[count]
-        const doc = {}
-        doc[`Token${letter}`] = {pattern: letter, line_breaks: true}
-        LD.mode(letter, doc)
+    it('should not accept a single mode definition after modes have been defined', function() {
+      function mixedbuild() {
+        LD.mode('A', { TokenA: 'A' })
+        LD.mode('B', { TokenA: 'B' })
+        LD.build({ TokenC: 'C' })
       }
-      const lexer = LD.build()
-      expect(Object.keys(lexer).length).toEqual(number)
+      expect(mixedbuild).toThrowError(Errors.BuildError)
     })
   })
 
-  it('should detect dependency loops', function() {
-    function catloop() {
-      LD.build({
-        Foo: {pattern: LD.CATEGORY, categories: 'Bar'},
-        Bar: {pattern: LD.CATEGORY, categories: 'Baz'},
-        Baz: {pattern: LD.CATEGORY, categories: 'Foo'}
+  describe('multi-mode', function() {
+    it('should require at least two modes', function() {
+      function onemode() {
+        LD.mode('A', { TokenA: 'A' })
+        LD.setDefault('A')
+        LD.build()
+      }
+      expect(onemode).toThrowError(Errors.BuildError)
+    })
+
+    it('should ensure that the default mode is valid', function() {
+      function invalidDefault() {
+        LD.mode('A', { TokenA: {pattern: 'A', line_breaks: true} })
+        LD.mode('B', { TokenB: 'B' })
+        LD.setDefault('C')
+        LD.build()
+      }
+      expect(invalidDefault).toThrowError(Errors.BuildError)
+    })
+
+    it('should support multiple modes', function() {
+      const numbers = [2,3,4,10,20]
+      const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      numbers.forEach((number)=> {
+        LD = new Lexdoc()
+        LD.setDefault('A')
+        for (let count = 0; count < number; count++) {
+          const letter = letters[count]
+          const doc = {}
+          doc[`Token${letter}`] = {pattern: letter, line_breaks: true}
+          LD.mode(letter, doc)
+        }
+        const lexer = LD.build()
+        expect(Object.keys(lexer).length).toEqual(number)
       })
-    }
-    function altloop() {
-      LD.build({
-        Foo: {pattern: 'foo', longer_alt: 'Bar'},
-        Bar: {pattern: 'bar', longer_alt: 'Baz'},
-        Baz: {pattern: 'baz', longer_alt: 'Foo'}
-      })
-    }
-    expect(catloop).toThrowError(Errors.DependencyError)
-    expect(altloop).toThrowError(Errors.DependencyError)
+    })
   })
 
-  it('should detect missing dependencies', function() {
-    function missingcat() {
-      LD.build({
-        Foo: LD.CATEGORY,
-        Bar: LD.CATEGORY,
-        Bat: {pattern: 'bat', categories: 'Foo Bar Baz'}
-      })
-    }
-    function missingalt() {
-      LD.build({ Foo: {pattern: 'foo', longer_alt: 'Bar'} })
-    }
-    expect(missingcat).toThrowError(Errors.DependencyError)
-    expect(missingalt).toThrowError(Errors.DependencyError)
+  describe('token set', function() {
+    it('should detect dependency loops', function() {
+      function catloop() {
+        LD.build({
+          Foo: {pattern: LD.CATEGORY, categories: 'Bar'},
+          Bar: {pattern: LD.CATEGORY, categories: 'Baz'},
+          Baz: {pattern: LD.CATEGORY, categories: 'Foo'}
+        })
+      }
+      function altloop() {
+        LD.build({
+          Foo: {pattern: 'foo', longer_alt: 'Bar'},
+          Bar: {pattern: 'bar', longer_alt: 'Baz'},
+          Baz: {pattern: 'baz', longer_alt: 'Foo'}
+        })
+      }
+      expect(catloop).toThrowError(Errors.DependencyError)
+      expect(altloop).toThrowError(Errors.DependencyError)
+    })
+
+    it('should detect missing dependencies', function() {
+      function missingcat() {
+        LD.build({
+          Foo: LD.CATEGORY,
+          Bar: LD.CATEGORY,
+          Bat: {pattern: 'bat', categories: 'Foo Bar Baz'}
+        })
+      }
+      function missingalt() {
+        LD.build({ Foo: {pattern: 'foo', longer_alt: 'Bar'} })
+      }
+      expect(missingcat).toThrowError(Errors.DependencyError)
+      expect(missingalt).toThrowError(Errors.DependencyError)
+    })
   })
 
 })
